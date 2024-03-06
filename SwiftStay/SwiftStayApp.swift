facd628e059d5a7e295f2c912e1aa245ca616172
Min Pyae <minmp2002@gmail.com>
2024-03-06 00:14:16 +0000

    Initial Commit

---
+++ SwiftStay/SwiftStayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStayApp.swift
+//  SwiftStay
+//
+//  Created by Chris Min on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
